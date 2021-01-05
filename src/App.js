7ead5ef2c75a71569f47958d37cdc01e147decf0
Staffan <id17svd@cs.umu.se>
2021-01-05 16:25:20 +0100

    warning fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import "./App.css";
-import Map from './pages/Map'
-import Login from './pages/Login'
-import Landing from './pages/Landing'
-import CheckIn from './pages/CheckIn'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-
-const axios = require("axios");
+import './App.css';
+import Map from './pages/Map';
+import Login from './pages/Login';
+import Landing from './pages/Landing';
+import CheckIn from './pages/CheckIn';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Landing}/>
-            <Route path="/login" exact component={Login}/>
-            <Route path="/checkin" exact component={CheckIn}/>
-            <Route path="/map" exact component={Map}/>
-        </Switch>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/checkin" exact component={CheckIn} />
+        <Route path="/map" exact component={Map} />
+      </Switch>
     </Router>
-
-
-   
   );
 }
 
